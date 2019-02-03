ca2cf35c6970a6757fb718dd2b2adb1bbb082f3d
Frixu <frixu@frixu.me>
2019-02-03 19:28:02 +0100

    App will try to use env vars

--- Bot.fs
+++ Bot.fs
@@ -1,6 +1,7 @@
 ﻿module Bot 
 
     open DSharpPlus
+    open System
     open System.IO
     open System.Threading.Tasks
     open Commands
@@ -9,9 +10,20 @@
     botconf.set_AutoReconnect true
     botconf.set_LogLevel LogLevel.Debug
     botconf.set_TokenType TokenType.Bot
-    File.ReadAllText "token.txt" |> botconf.set_Token
 
-    let bot = new DSharpPlus.DiscordClient(botconf)
+    let token = Environment.GetEnvironmentVariable "CONSENSUS_DISCORD_TOKEN"
+    match token with
+    | null ->
+        try
+            printfn "No token in environment variables. Searching token.txt."
+            File.ReadAllText "token.txt" |> botconf.set_Token
+        with
+            | :? FileNotFoundException -> 
+                printfn "No file named token.txt exists. Aborting now."
+                exit 1
+    | _ -> botconf.set_Token token
+
+    let bot = new DiscordClient(botconf)
 
     bot.add_MessageCreated (fun e -> 
         match e.Message.Content.ToLower() with
--- Program.fs
+++ Program.fs
@@ -1,8 +1,15 @@
 ﻿open Bot
+open DSharpPlus.Exceptions
 open System.Threading.Tasks
 
 [<EntryPoint>]
 let main _ =
-    bot.ConnectAsync() |> Async.AwaitTask |> Async.RunSynchronously
+    try
+        bot.ConnectAsync() |> Async.AwaitTask |> Async.RunSynchronously
+        printfn "Connected successfully."
+    with
+        | :? UnauthorizedException -> 
+            printfn "Invalid token. Aborting now."
+            exit 2
     Task.Delay(-1) |> Async.AwaitTask |> Async.RunSynchronously
     0
